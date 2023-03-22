976a9cb0ad3849e99918519af7b627c9b08b7d8f
Sweta Snigdha Sahoo <46675936+swetasahoo29@users.noreply.github.com>
2023-03-23 01:26:36 +0530

    Add files via upload

---
+++ addNumbers.java
@@ -0,0 +1,8 @@
+
+public class addNumbers {
+    public static void main(String[] args) {
+        int A = Integer.valueOf(args[0]);
+        int B = Integer.valueOf(args[1]);
+        System.out.println(A + B);
+    }
+}
