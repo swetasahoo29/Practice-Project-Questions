6b89d8a8f258bcbb12780904ae297405a7a2ac98
Sweta Snigdha Sahoo <46675936+swetasahoo29@users.noreply.github.com>
2023-03-25 20:50:22 +0530

    Update maximumCharacter.java

--- day2.java/src/maximumCharacter.java
+++ day2.java/src/maximumCharacter.java
@@ -1,24 +1,24 @@
 
 public class maximumCharacter {
 
-    public char getMaxOccurringChar(String str)
+    public char getMaxOccurringChar(String s)
     {
-        //Array with length = 256
+        //Array with length 256
         int count[] = new int[256];
  
         // Construct character count array from the input
-        int len = str.length();
-        for (int i = 0; i < len; i++)
-            count[str.charAt(i)]++;
+        int l = s.length();
+        for (int i = 0; i < l; i++)
+            count[s.charAt(i)]++;
  
-        int max = -1; // Initialize max count
+        int max = -1; // Initialize maximum count
         char result = ' '; // Initialize result
  
         // Traversing through the string and maintain the count of each character
-        for (int i = 0; i < len; i++) {
-            if (max < count[str.charAt(i)]) {
-                max = count[str.charAt(i)];
-                result = str.charAt(i);
+        for (int i = 0; i < l; i++) {
+            if (max < count[s.charAt(i)]) {
+                max = count[s.charAt(i)];
+                result = s.charAt(i);
             }
         }
  
@@ -30,10 +30,10 @@ public class maximumCharacter {
         // for(int i=0; i<255; i++) {
         //     System.out.println((char)i + " " + i);
         // } 
-        String str = "sample string";
-        maximumCharacter ms = new maximumCharacter();
-        System.out.println("Max occurring character is "
-                           + ms.getMaxOccurringChar(str));
+        String s = "sweta";
+        maximumCharacter m = new maximumCharacter();
+        System.out.println("Maximum occurring character : "
+                           + m.getMaxOccurringChar(s));
     }
 
 }
